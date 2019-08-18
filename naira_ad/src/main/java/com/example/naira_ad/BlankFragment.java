c8f9c1f596136e08c710cf49a8e80c970dd81c50
samrays <samchuksy@gmail.com>
2019-08-18 01:04:28 +0100

    naira_logo

--- naira_ad/src/main/java/com/example/naira_ad/BlankFragment.java
+++ naira_ad/src/main/java/com/example/naira_ad/BlankFragment.java
@@ -43,15 +43,16 @@ public class BlankFragment extends Fragment {
     }
 
     public void showAds(View v){
+
+        mDialog = new Dialog(getContext());
+        TextView txtclose;
+        mDialog.setContentView(R.layout.naira_popup);
+        mImageView = mDialog.findViewById(R.id.imageView2);
         Picasso.get()
                 .load("https://ads.adnaira.ng/assets/ads/ads-by-adnaira.png")
                 .fit()
                 .centerCrop()
                 .into(mImageView);
-        mDialog = new Dialog(getContext());
-        TextView txtclose;
-        mDialog.setContentView(R.layout.naira_popup);
-        mImageView = mDialog.findViewById(R.id.imageView2);
         txtclose =(TextView) mDialog.findViewById(R.id.txtclose);
 
         txtclose.setOnClickListener(new View.OnClickListener() {
